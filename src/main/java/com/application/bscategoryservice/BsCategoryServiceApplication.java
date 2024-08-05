297378682f5ccc97b3492cf7553adcc93caab38e
Yaroslav <loghitechdf@gmail.com>
2024-08-05 20:24:54 +0300

    Project init

---
+++ src/main/java/com/application/bscategoryservice/BsCategoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.application.bscategoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsCategoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsCategoryServiceApplication.class, args);
+    }
+
+}
